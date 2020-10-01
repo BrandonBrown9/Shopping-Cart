936ad41ecf0b0729e33e55b5813bcbbdc364bcc8
bran9230 <branjay18@yahoo.com>
2020-10-01 18:05:52 -0400

    Master Code
    Update this code with working components and code. Otherwise, branch the code.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// add to cart function
+
+// update cart
+    // quantities
+
+// renmove from cart function
+
+// calculate price function
+    // should add 7% tax to total
+
+// pagination ?
+
+// search for item
+    // display message if item doesn't exist
